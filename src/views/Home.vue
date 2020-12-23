b842b3f56e894a68e6ea11b7c93d5598ce9495d4
brian <riceb53@gmail.com>
2020-12-22 19:36:43 -0500

    get mapbox working in vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,30 @@
-
-
 <template>
   <div class="home">
+    <div id="map"></div>
     <h1>{{ message }}</h1>
   </div>
 </template>
 
 <style>
+	body { margin: 0; padding: 0; }
+	#map { position: absolute; top: 0; bottom: 0; width: 800px; height: 500px; }
 </style>
 
 <script>
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",      
     };
   },
-  created: function() {},
+  mounted: function() {
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+    center: [-86.7816, 36.1627], // starting position [lng, lat]
+    zoom: 10 // starting zoom
+    });
+  },
   methods: {}
 };
 </script>
