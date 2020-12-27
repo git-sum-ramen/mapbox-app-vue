234c1f96d5c776c14f6c20887910077e1444a0ca
brian <riceb53@gmail.com>
2020-12-27 17:29:07 -0500

    add multiple markers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,19 @@
 <style>
 	body { margin: 0; padding: 0; }
 	#map { position: absolute; top: 0; bottom: 0; width: 800px; height: 500px; }
+  #marker {
+background-image: url('https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg');
+background-size: cover;
+width: 50px;
+height: 50px;
+border-radius: 50%;
+cursor: pointer;
+}
+ 
+.mapboxgl-popup {
+max-width: 200px;
+}
+  
 </style>
 
 <script>
@@ -16,93 +29,44 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",      
+      message: "Welcome to Vue.js!", 
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" }
+      ]     
     };
   },
   mounted: function() {
+    var monument = [-77.0353, 38.8895];
+
     console.log(process.env.VUE_APP_MY_API_KEY);
     mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
     var map = new mapboxgl.Map({
       container: 'map', // container id
       // style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
       style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-      center: [-86.7816, 36.1627], // starting position [lng, lat]
+      center: monument, // starting position [lng, lat]
       zoom: 0 // starting zoom
     });
 
-    map.on('load', function () {
-    // We use D3 to fetch the JSON here so that we can parse and use it separately
-    // from GL JS's use in the added source. You can use any request method (library
-    // or otherwise) that you want.
-    d3.json(
-    'https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson',
-    function (err, data) {
-    if (err) throw err;
-    
-    // save full coordinate list for later
-    var coordinates = [
-      // nashville
-      [
-          -86.7816, 
-          36.1627
-      ],
-      // buffalo
-      [
-        -78.8784,
-          42.8864
-      ],
-      [
-        -122.4194,
-          37.7749
-      ],
-      [
-        -122.4194,
-          37.7749
-      ],
-      [
-        -87.6359,
-        41.8789
-      ]
-    ];
-    
-    // start by showing just the first coordinate
-    data.features[0].geometry.coordinates = [coordinates[0]];
-    
-    // add it to the map
-    map.addSource('trace', { type: 'geojson', data: data });
-    map.addLayer({
-    'id': 'trace',
-    'type': 'line',
-    'source': 'trace',
-    'paint': {
-    'line-color': 'yellow',
-    'line-opacity': 0.75,
-    'line-width': 8
-    }
-    });
-    
-    // setup the viewport
-    map.jumpTo({ 'center': coordinates[0], 'zoom': 2 });
-    map.setPitch(30);
-    
-    // on a regular basis, add more coordinates from the saved list and update the map
-    var i = 0;
-    var timer = window.setInterval(function () {
-        if (i < coordinates.length) {
-          data.features[0].geometry.coordinates.push(
-            coordinates[i]
-          );
-          map.getSource('trace').setData(data);
-          map.panTo(coordinates[i]);
-          i++;
-        } else {
-          window.clearInterval(timer);
-        }
-      }, 1000);
-      }
-      );
-    });
+    this.places.forEach(function(place) {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        place.description
+        );
+        
+        // create DOM element for the marker
+        var el = document.createElement('div');
+        el.id = 'marker';
+        
+        // create the marker
+        new mapboxgl.Marker(el)
+        .setLngLat([place.lng, place.lat])
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+      })
 
+
+    
   },
   methods: {}
 };
